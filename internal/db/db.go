b23f6fb9946c76e63a78a19b0174717fb4bb0ad6
caleboss <philanthropistcal@gmail.com>
2026-05-21 22:37:40 +0000

    Tried to rebuild the database connection from scratch again

--- internal/db/db.go
+++ internal/db/db.go
@@ -5,21 +5,40 @@ import (
 	"log"
 )
 
-// create a global database connection//
+// var DB *sql.DB
+
+// func Connect() {
+// 	var err error
+// 	//DSN==data sourcce name,how to find and connect to the database//s
+// 	dsn := "postgres://caleboss:__67myboss$12@localhost:5432/todoapp?sslmode=disable"
+// 	DB, err = sql.Open("pgx", dsn)
+// 	if err != nil {
+// 		log.Fatal("Database failed to open")
+
+// 	}
+
+// 	err = DB.Ping()
+// 	if err != nil {
+// 		log.Fatal("database failed to connect")
+
+// 	}
+
+// 	log.Println("database connected successfully")
+
+// }
 var DB *sql.DB
 
 func Connect() {
 	var err error
-	dsn := "postgres://postgres:password@localhost:5432/todoapp?sslmode=disable"
+	dsn := "postgres://caleboss:password@localhost:5432/todoapp?sslmode=disable"
 	DB, err = sql.Open("pgx", dsn)
 	if err != nil {
-		log.Fatal("Failed to open database")
+		log.Fatal("database failed to open")
 	}
 
 	err = DB.Ping()
 	if err != nil {
-		log.Fatal("DB not reachable")
+		log.Fatal("database failed to connect")
 	}
-	log.Println("connection to database successful")
-
+	log.Println("database connected successfully")
 }
