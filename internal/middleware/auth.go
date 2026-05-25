0a1909fcb623bcbbc482de77ef8a0aa154849bd4
caleboss <philanthropistcal@gmail.com>
2026-05-25 23:17:02 +0000

    feat:Add Auth middleware

---
+++ internal/middleware/auth.go
@@ -0,0 +1,56 @@
+package middleware
+
+import (
+	"net/http"
+	"strings"
+	"todo-app/internal/utils"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		//grab the full "Bearer  eyjhbg...from the request header"
+		authHeader := c.GetHeader("Authorization")
+
+		//if no token was sent at all block immediately//
+		if authHeader == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": "authorization header required",
+			})
+			c.Abort()
+			return
+		}
+
+		//split "Bearer eyjhbg...into["bearer","eyjhbg..."]//
+		parts := strings.Split(authHeader, "")
+		//must have exactly two parts and first part must be Bearer//
+
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": "invalid authorization format",
+			})
+			c.Abort()
+			return
+		}
+
+		//parts[1] is the raw token "eyjhbg..."
+		tokenString := parts[1]
+
+		//pass raw token to ValidateJWT to erify and extract UserId//
+		userId, err := utils.ValidateJWT(tokenString)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": "invalid or expired token",
+			})
+			c.Abort()
+			return
+		}
+		//store userId in context so handlers can access it//
+		c.Set("userId", userId)
+
+		//everything passed then let request through to handler//
+		c.Next()
+	}
+}
